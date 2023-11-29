7ed9e9f92b7b6f116c913fb05184f046d9e2dd46
Capde <capde22@gmail.com>
2023-11-29 18:22:13 -0300

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,32 @@
 import type { AppProps } from "next/app";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider, coinbaseWallet, embeddedWallet, metamaskWallet, smartWallet, walletConnect } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const activeChain = "mumbai";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const smartWalletConfig = {
+    factoryAddress: '0x25977D04c936D8855040738fCF4C874709Da997a',
+    gasless: true,
+  }
+  const customWallet = smartWallet(embeddedWallet({recommended: true}), smartWalletConfig);
+
+  customWallet.meta.name = 'Google'
+  customWallet.meta.iconURL = 'https://e7.pngegg.com/pngimages/344/344/png-clipart-google-logo-google-logo-g-suite-google-text-logo-thumbnail.png'
+
   return (
     <ThirdwebProvider
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
       activeChain={activeChain}
+      supportedWallets={[
+        customWallet,
+        metamaskWallet(),
+        coinbaseWallet(),
+        walletConnect(),
+      ]}
     >
       <Component {...pageProps} />
     </ThirdwebProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,106 +1,45 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, useAddress, useContract, useOwnedNFTs, useTokenBalance } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
-import Image from "next/image";
 import { NextPage } from "next";
+import { NFT_CONTRACT_ADDRESS, TOKEN_CONTRACT_ADDRESS } from "../constans/addresses";
 
 const Home: NextPage = () => {
+  const address = useAddress();
+
+  const {
+    contract: nftContract
+  } = useContract(NFT_CONTRACT_ADDRESS)
+
+  // const {
+  //   contract: tokenContract
+  // } = useContract(TOKEN_CONTRACT_ADDRESS)
+
+  const {
+    data: ownedNFTs,
+    isLoading: isNFTLoading,
+  } = useOwnedNFTs(nftContract, address)
+  // const {
+  //   data: tokenBalance,
+  //   isLoading: isTokenLoading,
+  // } = useTokenBalance(tokenContract, address)
+
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-        <div className={styles.header}>
-          <h1 className={styles.title}>
-            Welcome to{" "}
-            <span className={styles.gradientText0}>
-              <a
-                href="https://thirdweb.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                thirdweb.
-              </a>
-            </span>
-          </h1>
-
-          <p className={styles.description}>
-            Get started by configuring your desired network in{" "}
-            <code className={styles.code}>src/index.js</code>, then modify the{" "}
-            <code className={styles.code}>src/App.js</code> file!
-          </p>
-
-          <div className={styles.connect}>
-            <ConnectWallet
-              dropdownPosition={{
-                side: "bottom",
-                align: "center",
-              }}
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://portal.thirdweb.com/"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/portal-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText1}>Portal ➜</h2>
-              <p>
-                Guides, references, and resources that will help you build with
-                thirdweb.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/dashboard"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/dashboard-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText2}>Dashboard ➜</h2>
-              <p>
-                Deploy, configure, and manage your smart contracts from the
-                dashboard.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/templates"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/templates-preview.png"
-              alt="Placeholder preview of templates"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText3}>Templates ➜</h2>
-              <p>
-                Discover and clone template projects showcasing thirdweb
-                features.
-              </p>
-            </div>
-          </a>
-        </div>
+        <ConnectWallet />
+        {address && (
+          <>
+            {!isNFTLoading ? (
+              ownedNFTs && ownedNFTs.length > 0 ? (
+                <p>Total NFTs Owned: {ownedNFTs.length} </p>
+              ) : (
+                <p>Total Owned NFTs: 0 </p>
+              )
+            ) : (
+              <p>Loading...</p>
+            )}
+          </>
+        )}
       </div>
     </main>
   );
